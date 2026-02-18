36fb70c0d58fe4eea89bf1d21fa6c00ca1175e77
LuisT <Gutytrejo1502@gmail.com>
2026-02-17 20:18:39 -0800

    Implement rectangle area comparison
    Added functionality to compare areas of two rectangles based on user input.

--- Assignment1_RectangleArea.cpp
+++ Assignment1_RectangleArea.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
-	
+    double length_1, width_1, length_2, width_2;
+    //input for first rectangle
+    cout << "Enter the length and width of the first rectangle: ";
+    cin  >> length_1 >> width_1;
+
+    //input for second rectangle
+    cout <<"Enter the length and width of the second rectangle:";
+    cin  >> length_2 >> width_2;
+
+    //calculate areas
+    double area_1 = length_1 * width_1;
+    double area_2 = length_2 * width_2;
+
+    //comparison and output
+    if (area_1 > area_2)
+       { 
+        cout << "The first rectangle has the greater area." << endl;
+       }
+        else if (area_1 < area_2)
+        {
+            cout << "The second rectangle has the greater area." << endl;
+        }
+        else if (area_1 == area_2)
+        {
+            cout << "The areas are the same." << endl;
+        }
+
     return 0;
 }
-
