6ebba6ff2239a082ebfafe3aeba3c9043e9f88fd
LuisT <Gutytrejo1502@gmail.com>
2026-02-21 22:32:07 -0800

    Implement discount calculation for software purchases
    Added discount calculation and total cost output for software packages.

--- SoftwareStore.cpp
+++ SoftwareStore.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+
 int main()
+
 {
-    double mph = 6.5;
-    // PACE calculation
-    /* write your code */
+    const double price = 199.00;
+    double discount, total;
+    int quantity;
 
-    //express pace in minutes and seconds
-  /* write your code here */
+    cout << "Enter the quantity of software packages: ";
+    cin >> quantity;
+    cout << "You entered: " << quantity << endl;
 
-  //express your output in 1 decimal points
+    //calculate discount
+    if (quantity >= 10 && quantity <= 19)
+        discount = 0.20;
+    else if (quantity >= 20 && quantity <= 49)
+        discount = 0.30;
+    else if (quantity >= 50 && quantity <= 99)
+        discount = 0.40;
+    else if (quantity >= 100)
+        discount = 0.50;
+        else discount = 0.0;
 
+    //check out
+    total = quantity * price * (1-discount);
+    cout << fixed << setprecision(2);
+    cout << "Total cost: $" << total << endl;
+
+    return 0;
 
 }
